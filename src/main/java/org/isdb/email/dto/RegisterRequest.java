71832f0c8b1551073b5a047610b409a39d872aa6
Farzana Akter <farzana.nilu99@gmail.com>
2025-04-21 13:32:06 +0600

    last

--- src/main/java/org/isdb/email/dto/RegisterRequest.java
+++ src/main/java/org/isdb/email/dto/RegisterRequest.java
@@ -1,12 +1,14 @@
 package org.isdb.email.dto;
 
+import org.isdb.email.constants.Role;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
 
-		@NotBlank(message = "Email cannot be blank") @Email(message = "Email should be valid") String email,
+		@NotBlank(message = "Email cannot be blank") @Email(message = "Email should be valid") String email, Role role,
 
 		@NotBlank(message = "Password cannot be blank") @Size(min = 6, message = "Password must be at least 6 characters") String password,
 
